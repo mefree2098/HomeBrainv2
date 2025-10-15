581d627571e4d1244dacc8b666838f7d4178123e
HomeBrain Maintainers <maintainers@example.com>
2025-10-14 18:21:07 -0600

    another one

--- client/src/api/remoteUpdates.ts
+++ client/src/api/remoteUpdates.ts
@@ -1,26 +1,26 @@
 import api from './api';
 
 export async function getPackageInfo() {
-  const res = await api.get('/remote-updates/package-info');
+  const res = await api.get('/api/remote-updates/package-info');
   return res.data;
 }
 
 export async function generatePackage() {
-  const res = await api.post('/remote-updates/generate-package');
+  const res = await api.post('/api/remote-updates/generate-package');
   return res.data;
 }
 
 export async function initiateUpdate(deviceId: string) {
-  const res = await api.post(`/remote-updates/initiate/${deviceId}`);
+  const res = await api.post(`/api/remote-updates/initiate/${deviceId}`);
   return res.data;
 }
 
 export async function checkForUpdates(deviceId: string) {
-  const res = await api.get(`/remote-updates/check/${deviceId}`);
+  const res = await api.get(`/api/remote-updates/check/${deviceId}`);
   return res.data;
 }
 
 export async function getUpdateStats() {
-  const res = await api.get('/remote-updates/statistics');
+  const res = await api.get('/api/remote-updates/statistics');
   return res.data;
 }
